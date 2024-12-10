50c55de728716bede60ba4566410a3bd2e147d90
aclj <ariana.lopez.j@uni.pe>
2024-12-09 20:23:33 -0500

    Maquetado de 4 rutas: Asistencia en vivo, Listado de Sesiones, Menú del Estudiante, Historial de un Curso

--- src/app/features/qrAttendance/pages/classrooms/classrooms.component.ts
+++ src/app/features/qrAttendance/pages/classrooms/classrooms.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-classrooms',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './classrooms.component.html',
   styleUrl: './classrooms.component.css'
 })
 export class ClassroomsComponent {
-
+  cursos = [
+    "Ingeniería de Software",
+    "Computación centrada en Redes",
+    "Análisis y Modelamiento Numérico",
+    "Diseño de Algoritmos"
+  ]
 }
